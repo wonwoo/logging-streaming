c106acb700f43d0b9979a310438fd4fc3dcb1070
wonwoo <aoruqjfu@gmail.com>
2017-08-14 12:12:21 +0900

    오프셋 추가 일단 주석

--- src/main/scala/KafkaStreaming.scala
+++ src/main/scala/KafkaStreaming.scala
@@ -1,10 +1,9 @@
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.SparkContext
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 
@@ -21,6 +20,7 @@ object KafkaStreaming {
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
+
     val sparkConf = new SparkConf().setAppName("KafkaStreaming").setMaster("local[*]")
     val sc = new SparkContext(sparkConf)
     val ssc = new StreamingContext(sc, Seconds(10))
@@ -30,6 +30,14 @@ object KafkaStreaming {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
+
+//    stream.foreachRDD { rdd =>
+//      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+//      rdd.foreachPartition { iter =>
+//        val o: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+//        println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}" )
+//      }
+//    }
 
     //TODO where data save ?
     val map = stream.map(record => (record.key, record.value))
